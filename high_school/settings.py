d1136dcc16b257dd277b9aa2491763d731e064e9
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2023-08-24 19:01:50 +0500

    24.08.2023
    19:01

--- high_school/settings.py
+++ high_school/settings.py
@@ -113,17 +113,23 @@ CKEDITOR_CONFIGS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'litsey',
-        'USER': 'litsey',
-        'PASSWORD': 'Qosim123!',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'litsey',
+#         'USER': 'litsey',
+#         'PASSWORD': 'Qosim123!',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
